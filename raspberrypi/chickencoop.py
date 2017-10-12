c5f073d4ff78109b28828bb41cad8b50fad191de
Jonathan Cutting <joncutting@gmail.com>
2017-10-11 21:23:13 -0400

    Added weather thread. Stabilized IDLE development and logging. Got locks working to sync access to client and to ioStatus.

--- raspberrypi/chickencoop.py
+++ raspberrypi/chickencoop.py
@@ -1,28 +1,37 @@
 #To use this program run the following from admin command prompt:
 #pip install flask pymodbus
 #
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os.path
 import requests
 import random
 import pickle
 import atexit
-from threading import Thread, Lock
+from threading import Thread, Lock, Event
 import time
 import datetime
 from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 from pymodbus.exceptions import ModbusIOException
 import logging
 
-ioStatus = {'tempC':0.0}
+ioStatus = {'tempC':0.0,
+            'heaterPctCmd':0,
+            'heaterPctOut':0,
+            'lightPctCmd':0,
+            'lightPctOut':0,
+            'doorCmd':0,
+            'doorStatus':0}
+
 ioStatusLock = Lock()
+stopNow = Event()
 
 #Set up logging
 logging.basicConfig()
 log = logging.getLogger()
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 
-#Set up Modbus client to talk to Arduino
+#Set up Modbus client to talk to Arduino. Be sure to use port by ID so we always
+#get the right one (it can switch from /dev/ttyUSB0 to /dev/ttyUSB1 without warning)
 client = ModbusClient(method='rtu',
                       port='/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AH05HBFT-if00-port0',
                       timeout=1,
@@ -30,16 +39,25 @@ client = ModbusClient(method='rtu',
                       stopbits = 1,
                       bytesize = 8,
                       parity = 'N')
+clientLock = Lock()
 client.connect()
+
+weather = None
 
 #Set up web server
 app = Flask(__name__)
 
 def backgroundLoop():
-    print ("Background loop started.")
-    while True:
+    log.info("Background loop started.")
+    while not stopNow.wait(1):
         doModbus()
-        time.sleep(1)
+
+def weatherLoop():
+    global weather
+    log.info("Weather thread started")
+    weather = getWeather()
+    while not stopNow.wait(600):
+        weather = getWeather()
         
 def doorCommand(number):
     pass
@@ -56,10 +74,10 @@ def saveSettings():
 
 def loadSettings():
     if os.path.isfile("save.p"):
-        print ("loading settings from save.p")
+        log.info("loading settings from save.p")
         return pickle.load( open( "save.p", "rb" ) )
     else:
-        print ("loading default settings")
+        log.info("loading default settings")
         return  {
             'WOEID' : "2396147"
         }
@@ -80,64 +98,114 @@ def getWeather():
     return r.json()['query']['results']['channel']
 
 def getOutsideCondition():
-    lookup = getWeather()
-    return lookup['item']['condition']['text']
+    return weather['item']['condition']['text']
 
 def getInternalTemperature():
-    ioStatusLock.acquire()
-    t = ioStatus['tempC']
-    ioStatusLock.release()
+    with ioStatusLock:
+        #We're thread protected in here
+        t = ioStatus['tempC']
     return t
 
 def getExternalTemperature():
-    lookup = getWeather()
-    return int(lookup['item']['condition']['temp'])
+    return int(weather['item']['condition']['temp'])
 
 def getSunrise():
-    lookup = getWeather()
-    return lookup['astronomy']['sunrise']
+    return weather['astronomy']['sunrise']
 
 def getSunset():
-    lookup = getWeather()
-    return lookup['astronomy']['sunset']
+    return weather['astronomy']['sunset']
 
 
 
 #Modbus memory map for communication with slave
-#Address   Description
-#1         Motor enable (0=off, 1=on)
-#2         Heater power (0-100%)
-#3         Door Status (0=unknown, 1=opening, 2=closing, 3=open, 4=closed)
-#4         Door Command (0=done, 1=open, 2=close)
-#5         Lighting power (0-100%)
-#6         Auger turns remaining (0-100)
-#7         Temperature (0-120) degrees F
+#0  TEMPC10,        //0-600C * 10 = degrees C x 10
+#1  HEATER_PCT_CMD,  //0-100%
+#2  HEATER_PCT_OUT,  //0-100%
+#3  LIGHT_PCT_CMD,  //0-100%
+#4  LIGHT_PCT_OUT,  //0-100%
+#5  DOOR_CMD,
+#6  DOOR_STATUS,
+#7  TOTAL_ERRORS,
 
 def doModbus():
     try:
-        rr = client.read_holding_registers(0, 8, unit=1)
-        log.debug("Modbus got {}".format(rr.registers))
-        ioStatusLock.acquire()
-        ioStatus['tempC'] = rr.registers[0]/10.0
-        ioStatusLock.release()
+        #Read 8 holding registers from address 0
+        with clientLock:
+            #We're thread protected in here
+            rr = client.read_holding_registers(0, 8, unit=1)
+            #Make sure the read was successful
+            assert(rr.function_code < 0x80)
+            
+        with ioStatusLock:
+            #We're thread protected in here
+            #Copy the read values
+            ioStatus['tempC'] = rr.registers[0]/10.0
+            ioStatus['heaterPctOut'] = rr.registers[2]
+            ioStatus['lightPctOut'] = rr.registers[4]
+            ioStatus['doorStatus'] = rr.registers[6]
+            ioStatus['totalErrors'] = rr.registers[7]
+
     except Exception as e:
         log.exception(e)
+
+#Sets ioStatus key/value pair and writes to specified Modbus register
+def writeRegister(key, value, addr):
+    try:
+        with ioStatusLock:
+            #We're thread protected in here
+            ioStatus[key] = value
+        #Write the register
+        with clientLock:
+            #We're thread protected in here
+            rq = client.write_register(addr,value,unit=1)
+            assert(rq.function_code < 0x80)
+    except Exception as e:
+        log.exception(e)
+
+def setHeaterPct(value):
+    writeRegister('heaterPctCmd',value,1)
+
+def setLightPct(value):
+    writeRegister('lightPctCmd',value,3)
+
+def setDoor(value):
+    writeRegister('doorCmd',value,5)
        
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    templateData = {
-        'outsidecondition' : getOutsideCondition(),
-        'internaltemperature': "{}".format(getInternalTemperature()),
-        'externaltemperature' : "{}".format(getExternalTemperature()),
-        'sunrise': getSunrise(),
-        'sunset': getSunset(),
-        'title' : 'The Cutting Coop'
-        }
+    with ioStatusLock:
+        templateData = {
+            'outsidecondition' : getOutsideCondition(),
+            'internaltemperature': "{}".format(ioStatus['tempC']),
+            'externaltemperature' : "{}".format(getExternalTemperature()),
+            'sunrise': getSunrise(),
+            'sunset': getSunset(),
+            'doorStatus': ioStatus['doorStatus'],
+            'heaterPctOut': ioStatus['heaterPctOut'],
+            'lightPctOut': ioStatus['lightPctOut'],
+            'title' : 'The Cutting Coop'
+            }
+    
+    #Check button presses
+    for s in request.values:
+        app.logger.info(s)
+    if 'doorOpen' in request.values:
+        app.logger.info("Button clicked: Open door")
+        setDoor(1)
+    elif 'doorClose' in request.values:
+        app.logger.info("Button clicked: Close door")
+        setDoor(2)
+
     return render_template('main.html', **templateData)
 
 if __name__ == "__main__":
     t = Thread(target=backgroundLoop)
+    w = Thread(target=weatherLoop)
+    w.start()
     t.start()
-    app.run(host='0.0.0.0', port=8080, debug=True)
-
+    app.run(host='0.0.0.0', port=8080, debug=True, use_reloader=False)
+    #All done
+    stopNow.set()
+    t.join()
+    w.join()
     
